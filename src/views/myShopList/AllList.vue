c730a88275bcde095d717732ce3393ccfe2324ac
TripleKay <triplekay.k3@gmail.com>
2022-05-01 17:44:42 +0630

    first commit

---
+++ src/views/myShopList/AllList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'AllList',
+        data () {
+            return {
+                title: "This is all list",
+            }
+        },
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
